357e4278c5ba49e39bfbb80c05eaa4d7a1311914
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2022-09-16 09:10:23 -0500

    task complete

--- index.js
+++ index.js
@@ -15,11 +15,33 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person( name, age) {
+this.name = name ;
+this.age = age ;
+this.stomach = [] ;
 
 }
 
+Person.prototype.eat = function ( someFood ) {
+if ( this.stomach.length <= 10 ) {
+ return this.stomach.push ( someFood )
+}
+} 
 
+Person.prototype.poop = function () {
+ return this.stomach = []
+}
+
+Person.prototype.toString = function () {
+  return `${this.name},${this.age}`
+}
+
+const person = new Person ( "Maria", 50 )
+
+console.log (person)
+console.log (person.eat("Tomatos"))
+console.log (person.stomach)
+console.log (person.poop())
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -36,10 +58,24 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car( model, milesPerGallon) {
+
+  this.model = model ;
+  this.milesPerGallon = milesPerGallon ;
+  this.tank = 0 ;
+  this.odometer = 0; 
 
 }
 
+Car.prototype.fill = function (gallons) {
+   this.tank = this.tank + gallons
+   return this.tank
+}
+
+let car = new Car ( "Toyota", 5 )
+
+console.log (car)
+console.log (car.fill (5))
 
 /*
   TASK 3
@@ -49,11 +85,27 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby( name, age, favoriteToy ) {
+  this.name = name ;
+  this.age = age ;
+  this.stomach = [] ;
+  this.favoriteToy = favoriteToy
 }
 
+Baby.prototype = Object.create (Person.prototype)
 
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`
+}
+
+let baby = new Baby ("Anastasia", 1 , "Ball" ) 
+
+
+console.log (baby.name)
+console.log (baby.age )
+console.log (baby.favoriteToy)
+console.log (baby.play () )
+console.log (baby.eat)
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
